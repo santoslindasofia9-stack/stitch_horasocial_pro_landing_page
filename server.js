9b029fe815c8368c056794ca012840cea1a4cd1e
santoslindasofia9-stack <santoslindasofia9@gmail.com>
2026-06-20 22:33:11 -0500

    a

--- server.js
+++ server.js
@@ -272,9 +272,7 @@ app.get('/admin/mensajes', (req, res) => {
     res.sendFile(path.join(__dirname, 'stitch_horasocial_pro_landing_page', 'chat_con_maestro_soporte_de_inasistencia_1', 'code.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor de Horas Sociales corriendo en http://localhost:${PORT}`);
-});
+
 app.get('/admin/alertas', (req, res) => {
     res.sendFile(path.join(__dirname, 'stitch_horasocial_pro_landing_page', 'control_de_estudiantes_y_alertas_admin_versi_n_corregida', 'code.html'));
 });
